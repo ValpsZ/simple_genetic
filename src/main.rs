c5750dd732288b7187b64053dc5f1596d3daf6d5
ValpsZ <limelgren@gmail.com>
2023-11-23 21:04:00 +0100

    finnished genetic part and added a test in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use rand::prelude::*;
 
 struct Agent {
@@ -8,7 +10,7 @@ struct Agent {
     input_neurons: u32,
     hidden_neurons: u32,
     output_neurons: u32,
-    mutaion_rate: f64,
+    mutation_rate: f64,
 }
 
 struct Genome {
@@ -66,6 +68,17 @@ impl Genome {
     }
 }
 
+impl Clone for Genome {
+    fn clone(&self) -> Self {
+        Self {
+            gene_list: self.gene_list.clone(),
+            input_neurons: self.input_neurons,
+            hidden_neurons: self.hidden_neurons,
+            output_neurons: self.output_neurons,
+        }
+    }
+}
+
 impl Agent {
     fn calculate(&mut self) -> () {
         for i in 0..(&self.genome.gene_list).len() {
@@ -104,20 +117,12 @@ impl Agent {
             }
 
             if first_layer_index == 0 {
-                println!(
-                    "Value: {}",
-                    &self.input_list[first_neuron_index as usize] * weight
-                );
                 self.set_value(
                     &self.input_list[first_neuron_index as usize] * weight,
                     second_neuron_index as usize,
                     second_layer_index,
                 );
             } else {
-                println!(
-                    "Value: {}",
-                    &self.hidden_list[first_neuron_index as usize] * weight
-                );
                 self.set_value(
                     &self.hidden_list[first_neuron_index as usize] * weight,
                     second_neuron_index as usize,
@@ -142,7 +147,7 @@ impl Agent {
         let mut result: u32 = 0;
         for i in 0..31 {
             let rand: f64 = rng.gen();
-            if rand < self.mutaion_rate {
+            if rand < self.mutation_rate {
                 result = result | 0x1 << i;
             }
         }
@@ -169,31 +174,97 @@ impl Agent {
             input_neurons: self.input_neurons,
             hidden_neurons: self.hidden_neurons,
             output_neurons: self.output_neurons,
-            mutaion_rate: self.mutaion_rate,
+            mutation_rate: self.mutation_rate,
         }
     }
 }
 
+impl Clone for Agent {
+    fn clone(&self) -> Self {
+        Self {
+            genome: self.genome.clone(),
+            input_list: self.input_list.clone(),
+            hidden_list: self.hidden_list.clone(),
+            output_list: self.output_list.clone(),
+            input_neurons: self.input_neurons,
+            hidden_neurons: self.hidden_neurons,
+            output_neurons: self.output_neurons,
+            mutation_rate: self.mutation_rate,
+        }
+    }
+}
+
+fn create_agents(amount: usize, genome_length: usize, input_size: u32, hidden_size: u32, output_size: u32, mutation_rate: f64) -> Vec<Agent> {
+    let mut result: Vec<Agent> = Vec::with_capacity(amount);
+    for _ in 0..amount {
+        let new_agent = Agent {
+            genome: Genome{
+                gene_list: vec![0; genome_length],
+                input_neurons: input_size,
+                hidden_neurons: hidden_size,
+                output_neurons: output_size,
+            },
+            input_list: vec![1.0, 0.0, 0.0, 0.0, 0.0],
+            hidden_list: vec![0.0, 0.0, 0.0, 0.0, 0.0],
+            output_list: vec![0.0, 0.0, 0.0, 0.0, 0.0],
+            input_neurons: input_size,
+            hidden_neurons: hidden_size,
+            output_neurons: output_size,
+            mutation_rate: mutation_rate,
+        };
+        result.push(new_agent)
+    }
+    result
+}
+
+fn fitness(result: Vec<f32>, expected: &Vec<f32>) -> f32 {
+    result.into_iter().zip(expected).map(|(r, e)| f32::powf(r-e,2.0)).sum::<f32>()
+}
+
 fn main() {
-    let mut test_genome = Genome {
-        gene_list: vec![0b00000000000000000111111000000000u32],
-        input_neurons: 5,
-        hidden_neurons: 5,
-        output_neurons: 5,
-    };
+    let mut agent_list: Vec<Agent> = create_agents(10, 5, 1, 5, 5, 0.01);
+    let goal: Vec<f32> = vec![0.0, 0.0, 1.0, 0.0, 0.0];
+    let mut best_fittnes: f32 = 100.0;
 
-    let mut test_agent = Agent {
-        genome: test_genome,
-        input_list: vec![1.0, 0.0, 0.0, 0.0, 0.0],
-        hidden_list: vec![0.0, 0.0, 0.0, 0.0, 0.0],
-        output_list: vec![0.0, 0.0, 0.0, 0.0, 0.0],
-        input_neurons: 5,
-        hidden_neurons: 5,
-        output_neurons: 5,
-        mutaion_rate: 0.05,
-    };
+    for generation in 0..40 {
+        println!("Generation: {generation}");  
 
-    test_agent.genome.print_brain();
-    let test_agent_2 = test_agent.reproduce();
-    test_agent_2.genome.print_brain();
+        let new_agent_list: Vec<Agent> = agent_list.iter()
+                                                    .map(|x| x.reproduce())
+                                                    .collect::<Vec<Agent>>();
+        
+        agent_list = new_agent_list;
+
+        for i in 0..agent_list.len() {
+            agent_list[i].set_value(1.0, 0, 0);
+        }
+
+        let mut fittnes_list: Vec<f32> = Vec::with_capacity(agent_list.len());
+
+        for i in 0..agent_list.len() {
+            agent_list[i].calculate();
+            let f: f32 = fitness(agent_list[i].output_list.clone(), &goal);
+            if f < best_fittnes {
+                agent_list[i].genome.print_brain();
+                best_fittnes = f;
+            }
+            println!("Agent: {i} Fittnes: {f}");
+            fittnes_list.push(f);
+        }
+
+        let avg_fittness: f32 = fittnes_list.iter().sum::<f32>() / fittnes_list.len() as f32;
+        println!("Average fittness: {avg_fittness}");
+        
+        for i in (0..(agent_list.len()-1)).rev() {
+            if fittnes_list[i] > avg_fittness {
+                agent_list.remove(i);
+            }
+        }
+
+
+        while agent_list.len() < fittnes_list.len() {
+            agent_list.push(agent_list[(rand::random::<f32>() * agent_list.len() as f32).floor() as usize].clone());
+        }
+    }
+    println!("{:?}", agent_list[0].output_list);
 }
