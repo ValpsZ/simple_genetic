760ac9ebbde3fe8d2a5339c34ebd29fc6c9f0a46
ValpsZ <limelgren@gmail.com>
2024-05-15 10:14:23 +0200

    Add non-working test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,28 +1,61 @@
-use rand::Rng;
-use simple_genetic::simple_genetic::{activations, agent::{self, Agent}, fitness};
+#[cfg(test)]
+mod tests {
+    use rand::{thread_rng, Rng};
+    use simple_genetic::simple_genetic::{activations, agent::create_agents, fitness};
 
+    #[test]
+    fn example_use() {
+        // Constants
+        const GENERATIONS: usize = 100;
+        const NUM_AGENTS: usize = 10;
+        const GENOME_LENGTH: usize = 20;
+        const INPUT_SIZE: u32 = 1;
+        const HIDDEN_SIZE: u32 = 10;
+        const OUTPUT_SIZE: u32 = 1;
+        const MUTATION_RATE: f64 = 0.002;
 
-#[test]
-fn example_use() {
-    let generations = 100;
-    let num_agents: usize = 10; // Has to be even, else repoulation fails
-    let mut agents: Vec<Agent> =
-        agent::create_agents(num_agents, 20, 1, 10, 1, 0.002, activations::TANH, activations::TANH, fitness::MEAN_SQUARED);
+        // Create agents
+        let mut agents = create_agents(
+            NUM_AGENTS, // Initial population size
+            GENOME_LENGTH,
+            INPUT_SIZE,
+            HIDDEN_SIZE,
+            OUTPUT_SIZE,
+            MUTATION_RATE,
+            activations::TANH,
+            activations::TANH,
+            fitness::MEAN_SQUARED,
+        );
 
-    for _ in 0..generations {
-        let mut rng = rand::thread_rng();
-        let rand_f32: f32 = rng.gen_range(0.0..1.0);
-        let input = f32::sin(rand_f32);
-        for idx in 0..agents.len() {
-            agents[idx].set_value(input, 0, 0);
-            agents[idx].calculate();
-            agents[idx].calculate_fitness(vec![rand_f32]);
+        // Training loop
+        for generation in 0..GENERATIONS {
+            let input_value = thread_rng().gen_range(-std::f32::consts::PI..std::f32::consts::PI);
+            let expected_output = vec![input_value.sin()];
+
+            for agent in &mut agents {
+                agent.set_value(input_value, 0, 0);
+                agent.calculate();
+                agent.calculate_fitness(expected_output.clone());
+            }
+
+            agents.sort_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
+            let best_fittness = agents[0].fitness;
+            let selected_agents = agents
+                .iter()
+                .take(NUM_AGENTS / 2)
+                .cloned()
+                .collect::<Vec<_>>();
+
+            let mut new_population = Vec::new();
+            for idx in 0..NUM_AGENTS {
+                let parent = &selected_agents[idx];
+                let child = parent.reproduce();
+                new_population.push(child);
+            }
+            agents = new_population;
+
+            println!("Generation {}: Best fitness: {}", generation, best_fittness);
         }
-
-        agents.sort_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
-
-        agents.drain((num_agents / 2)..);
-
-
+        agents[0].genome.print_brain();
     }
 }
